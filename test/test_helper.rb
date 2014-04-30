98357a884d4dab765259e737230eb96fbaa00ea5
Jason Haruska <jason@haruska.com>
2014-04-30 15:09:30 -0500

    add tests for marshalled trees

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,4 +4,21 @@ Coveralls.wear!
 require 'binary_decision_tree'
 require 'minitest/autorun'
 
+class MiniTest::Spec
+  def assert_same_node(exp, act, msg = nil)
+    assert_equal exp.decision, act.decision, msg
+    assert_equal exp.slot, act.slot, msg
+  end
+
+  def assert_same_tree(exp, act, msg = nil)
+    assert_equal exp.depth, act.depth, msg
+    exp.size.times do |i|
+      next if i == 0
+      exp_node = exp.at(i)
+      act_node = act.at(i)
+      assert_same_node exp_node, act_node, msg
+    end
+  end
+end
+
 
---
+++ test/unit/binary_decision_tree/marshalled_tree_test.rb
@@ -0,0 +1,28 @@
+require_relative '../../../test/test_helper'
+
+module BinaryDecisionTree
+  class MarshalledTreeTest < MiniTest::Spec
+    before do
+      @tree = Tree.new(6)
+      @tree.size.times do |i|
+        next if i == 0
+        @tree.at(i).decision = rand(2)
+      end
+    end
+
+    describe "marshalling a tree" do
+      before do
+        @marshalled_tree = MarshalledTree.from_tree(@tree)
+      end
+
+      it "should marshal and unmarshal to the same tree" do
+        assert_same_tree @tree, @marshalled_tree.to_tree
+      end
+
+      it "should be able to unmarshal from numeric values" do
+        another_tree = MarshalledTree.new(@tree.depth, @marshalled_tree.decisions, @marshalled_tree.mask).to_tree
+        assert_same_tree @tree, another_tree
+      end
+    end
+  end
+end
